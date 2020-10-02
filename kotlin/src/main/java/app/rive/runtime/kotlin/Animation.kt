7fc023a6c352ad685723c5b2f4c934558fa0570e
Umberto Sonnino <usonnino@gmail.com>
2020-10-02 17:12:46 +0100

    Add a few getters for animation instances

--- kotlin/src/main/java/app/rive/runtime/kotlin/Animation.kt
+++ kotlin/src/main/java/app/rive/runtime/kotlin/Animation.kt
@@ -2,13 +2,28 @@ package app.rive.runtime.kotlin
 
 
 class Animation {
-    var nativePointer: Long
+    val nativePointer: Long
+
+    external private fun nativeName(nativePointer: Long): String
+    external private fun nativeDuration(nativePointer: Long): Int
+    external private fun nativeFps(nativePointer: Long): Int
+    external private fun nativeWorkStart(nativePointer: Long): Int
+    external private fun nativeWorkEnd(nativePointer: Long): Int
+
+    val duration: Int
+        get() = nativeDuration(nativePointer)
+    val fps: Int
+        get() = nativeFps(nativePointer)
+    val workStart: Int
+        get() = nativeWorkStart(nativePointer)
+    val workEnd: Int
+        get() = nativeWorkEnd(nativePointer)
+    val name: String
+        get() = nativeName(nativePointer)
 
     constructor(_nativePointer: Long) : super() {
         nativePointer = _nativePointer
     }
-
-    external private fun nativeName(nativePointer: Long): String
 
     companion object {
         init {
@@ -16,7 +31,7 @@ class Animation {
         }
     }
 
-    fun name(): String {
-        return nativeName(nativePointer)
+    override fun toString(): String {
+        return "Animation $name\n- Duration$duration\n- fps $fps\n- workStart $workStart\n- workEnd $workEnd"
     }
 }
