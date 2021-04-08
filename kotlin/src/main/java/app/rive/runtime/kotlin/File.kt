e39a850fc9d03e2aec7209012099db20a361c565
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-04-08 18:06:36 +0100

    finalize open files to ensure we clean up c++ objects

--- kotlin/src/main/java/app/rive/runtime/kotlin/File.kt
+++ kotlin/src/main/java/app/rive/runtime/kotlin/File.kt
@@ -11,10 +11,14 @@ class File(bytes: ByteArray) {
 
     private external fun import(bytes: ByteArray, length: Int): Long
     private external fun nativeArtboard(nativePointer: Long): Long
+    private external fun nativeDelete(nativePointer: Long)
 
     fun artboard(): Artboard {
         return Artboard(
             nativeArtboard(nativePointer)
         )
+    }
+    protected fun finalize() {
+        nativeDelete(nativePointer)
     }
 }
