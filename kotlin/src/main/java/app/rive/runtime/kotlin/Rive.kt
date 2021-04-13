b3ef6ed2bcfb05d8f6857a9331741edcf0757f21
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-04-13 11:32:14 +0100

    update Rive docs

--- kotlin/src/main/java/app/rive/runtime/kotlin/Rive.kt
+++ kotlin/src/main/java/app/rive/runtime/kotlin/Rive.kt
@@ -3,6 +3,13 @@ package app.rive.runtime.kotlin
 object Rive {
     private external fun nativeInitialize()
 
+    /**
+     * Initialises Rive.
+     *
+     * This loads the c++ libraries required to use Rive objects and then
+     * updates the c++ environment with a pointer to the JavaVM so that
+     * it can interact with Java objects. 
+     */
     fun init() {
         System.loadLibrary("jnirivebridge")
         nativeInitialize()
