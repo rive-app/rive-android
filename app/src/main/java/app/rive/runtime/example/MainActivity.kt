80ef2b09353c9680abffd946d1f6dbaead4c5086
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-04-14 13:08:58 +0100

    update riveanimationview to use attrs

--- app/src/main/java/app/rive/runtime/example/MainActivity.kt
+++ app/src/main/java/app/rive/runtime/example/MainActivity.kt
@@ -4,32 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatToggleButton
-import app.rive.runtime.kotlin.core.Loop
-import app.rive.runtime.kotlin.core.Rive
 import app.rive.runtime.kotlin.RiveAnimationView
+import app.rive.runtime.kotlin.core.Rive
 
 class MainActivity : AppCompatActivity() {
 
     val animationView by lazy(LazyThreadSafetyMode.NONE) {
         findViewById<RiveAnimationView>(R.id.rive_animation)
     }
-    val animationView2 by lazy(LazyThreadSafetyMode.NONE) {
-        findViewById<RiveAnimationView>(R.id.rive_animation2)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Rive.init()
         setContentView(R.layout.activity_main)
-
-        animationView.setAnimation(R.raw.flux_capacitor)
-        animationView.setRepeatMode(Loop.LOOP)
-        animationView.start()
-
-
-        animationView2.setAnimation(R.raw.off_road_car_blog)
-        animationView2.setRepeatMode(Loop.LOOP)
-        animationView2.start()
 
         val togglePlayback = findViewById<AppCompatToggleButton>(R.id.toggle)
         togglePlayback.isChecked = animationView.isRunning
