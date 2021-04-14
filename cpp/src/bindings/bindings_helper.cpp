80e9044e1bca2d82a201102f0dfdab369f77c8be
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-04-14 13:06:05 +0100

    add helper to determine how much space an artboard is taking up once aligned

---
+++ cpp/src/bindings/bindings_helper.cpp
@@ -0,0 +1,35 @@
+#include "models/jni_renderer.hpp"
+#include "models/dimensions_helper.hpp"
+#include "helpers/general.hpp"
+#include <jni.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    using namespace rive_android;
+
+    JNIEXPORT void JNICALL Java_app_rive_runtime_kotlin_core_Rive_nativeCalculateRequiredBounds(
+        JNIEnv *env,
+        jobject thisObj,
+        jobject jfit,
+        jobject jalignment,
+        jlong availableBoundsRef,
+        jlong artboardBoundsRef,
+        jlong requiredBoundsRef)
+    {
+        auto fit = ::getFit(env, jfit);
+        auto alignment = ::getAlignment(env, jalignment);
+        rive::AABB *availableBounds = (rive::AABB *)availableBoundsRef;
+        rive::AABB *artboardBounds = (rive::AABB *)artboardBoundsRef;
+        rive::AABB *requiredBounds = (rive::AABB *)requiredBoundsRef;
+
+        ::DimensionsHelper helper;
+
+        helper.computeDimensions(
+            fit, alignment, *availableBounds, *artboardBounds, *requiredBounds);
+    }
+
+#ifdef __cplusplus
+}
+#endif
