45d1b1a96d1fe4943ffe740ca2adfd12da17ba0b
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-04-22 11:01:11 +0100

    moving enums into their own file, as it seems to make android studio a bit happier

---
+++ kotlin/src/main/java/app/rive/runtime/kotlin/core/Alignment.kt
@@ -0,0 +1,7 @@
+package app.rive.runtime.kotlin.core
+
+enum class Alignment {
+    TOP_LEFT, TOP_CENTER, TOP_RIGHT,
+    CENTER_LEFT, CENTER, CENTER_RIGHT,
+    BOTTOM_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT,
+}
