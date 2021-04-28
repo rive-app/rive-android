39f3a0ece857987d7ba426536bbad34fde1072e6
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-04-28 14:29:31 +0100

    added state machine instances and input instances

---
+++ kotlin/src/main/java/app/rive/runtime/kotlin/core/SMINumber.kt
@@ -0,0 +1,19 @@
+package app.rive.runtime.kotlin.core
+
+/**
+ * [SMINumber]s represents a boolean input for State Machines
+ */
+class SMINumber(cppPointer: Long) : SMIInput(cppPointer) {
+    private external fun cppValue(cppPointer: Long): Float
+    private external fun cppSetValue(cppPointer: Long, value: Float)
+
+    var value: Float
+        get() = cppValue(cppPointer)
+        set(value) {
+            cppSetValue(cppPointer, value)
+        }
+
+    override fun toString(): String {
+        return "SMINumber $name\n"
+    }
+}
