ff0137d76ca1bd4772f1a0b012be7e7635700da0
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-06-11 16:05:29 +0100

    add blend activity example

---
+++ app/src/main/java/app/rive/runtime/example/BlendActivity.kt
@@ -0,0 +1,24 @@
+package app.rive.runtime.example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import app.rive.runtime.kotlin.RiveAnimationView
+import app.rive.runtime.kotlin.core.Rive
+
+class BlendActivity : AppCompatActivity() {
+
+    private val animationViewAsset by lazy(LazyThreadSafetyMode.NONE) {
+        findViewById<RiveAnimationView>(R.id.blend)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.blend)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        animationViewAsset.destroy()
+    }
+}
