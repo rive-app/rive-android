93f599a53f2a9a6f562ab6e4704921aafb1646bf
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-09-27 16:59:43 +0100

    simplify simple activity

--- app/src/main/java/app/rive/runtime/example/SimpleActivity.kt
+++ app/src/main/java/app/rive/runtime/example/SimpleActivity.kt
@@ -7,14 +7,6 @@ import app.rive.runtime.kotlin.core.Rive
 
 class SimpleActivity : AppCompatActivity() {
 
-    private val animationViewAsset by lazy(LazyThreadSafetyMode.NONE) {
-        findViewById<RiveAnimationView>(R.id.simple_view_asset)
-    }
-
-//    private val animationViewNetwork by lazy(LazyThreadSafetyMode.NONE) {
-//        findViewById<RiveAnimationView>(R.id.simple_view_network)
-//    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.simple)
