45ee0fa4994db1a186ca48b9c1459d0e52840a0b
Umberto Sonnino <umberto@rive.app>
2021-10-22 14:16:24 +0100

    Cleanup pass on jni_renderer_skia

--- cpp/include/models/jni_renderer_skia.hpp
+++ cpp/include/models/jni_renderer_skia.hpp
@@ -40,8 +40,6 @@ namespace rive_android
     samples::WorkerThread<samples::HotPocketState> mHotPocketThread =
         {"HotPocket", samples::Affinity::Even};
 
-    // sk_sp<GrDirectContext> mContext = nullptr;
-    // SkSurface *mSurface = nullptr;
     ANativeWindow *nWindow = nullptr;
     rive::Artboard *mArtboard = nullptr;
     rive::LinearAnimationInstance *mInstance = nullptr;
@@ -115,7 +113,7 @@ namespace rive_android
                  threadState->surface =
                      eglCreateWindowSurface(threadState->display, threadState->config, window, NULL);
                  ANativeWindow_release(window);
-                 //  if (!threadState->makeCurrent(threadState->surface))
+
                  if (!threadState->createGrContext())
                  {
                    LOGE("Unable to eglMakeCurrent");
@@ -126,8 +124,7 @@ namespace rive_android
                  int width = ANativeWindow_getWidth(window);
                  int height = ANativeWindow_getHeight(window);
 
-                 LOGI("Set up window surface %dx%d", width, height);
-
+                //  LOGI("Set up window surface %dx%d", width, height);
                  SwappyGL_setWindow(window);
 
                  threadState->width = width;
@@ -144,44 +141,8 @@ namespace rive_android
                });
     }
 
-    // void surfaceSetup(samples::ThreadState *tState, int width, int height)
-    // {
-    //   int cWidth = tState->width;
-    //   int cHeight = tState->height;
-    //   LOGI("Setting up the surface?!");
-    //   if (cWidth != width || cHeight != height)
-    //   {
-    //     LOGI("From %dx%d\n\t to %dx%d", cWidth, cHeight, width, height);
-    //     tState->width = width;
-    //     tState->height = height;
-
-    //     auto get_proc = [](void *context, const char name[]) -> GrGLFuncPtr
-    //     {
-    //       return reinterpret_cast<GrGLFuncPtr>(
-    //           reinterpret_cast<JNIRendererSkia *>(context)->getProcAddress(name));
-    //     };
-    //     auto get_string =
-    //         reinterpret_cast<PFNGLGETSTRINGPROC>(getProcAddress("glGetString"));
-    //     auto c_version = reinterpret_cast<const char *>(get_string(GL_VERSION));
-    //     std::string version(c_version);
-    //     auto interface = version.find("OpenGL ES") == std::string::npos
-    //                          ? GrGLMakeAssembledGLInterface(this, get_proc)
-    //                          : GrGLMakeAssembledGLESInterface(this, get_proc);
-    //     if (!interface)
-    //     {
-    //       LOGE("Failed to find the interface version!?");
-    //     }
-    //     mContext = GrDirectContext::MakeGL(interface);
-    //     if (!mContext)
-    //     {
-    //       LOGE("Failed to make context!?");
-    //     }
-    //   }
-    // }
-
     void *getProcAddress(const char *name) const
     {
-      // LOGD("Getting proc address for %s", name);
       if (name == nullptr)
       {
         return nullptr;
@@ -277,7 +238,9 @@ namespace rive_android
     void draw(samples::ThreadState *threadState)
     {
       // Don't render if we have no surface
-      if (threadState->surface == EGL_NO_SURFACE)
+
+      auto gpuSurface = threadState->getSkSurface();
+      if (threadState->surface == EGL_NO_SURFACE || !gpuSurface)
       {
         // Sleep a bit so we don't churn too fast
         std::this_thread::sleep_for(50ms);
@@ -297,135 +260,33 @@ namespace rive_android
       }
       threadState->lastUpdate = std::chrono::steady_clock::now();
 
-      threadState->x += threadState->velocity * deltaSeconds;
+      int w = threadState->width;
+      int h = threadState->height;
 
-      if (threadState->x > 0.8f)
+      SkCanvas *gpuCanvas = gpuSurface->getCanvas();
+      float elapsed = -1.0f * deltaSeconds;
+      gpuCanvas->drawColor(SK_ColorTRANSPARENT, SkBlendMode::kClear);
+      if (mArtboard != nullptr)
       {
-        threadState->velocity *= -1.0f;
-        threadState->x = 1.6f - threadState->x;
-      }
-      else if (threadState->x < -0.8f)
-      {
-        threadState->velocity *= -1.0f;
-        threadState->x = -1.6f - threadState->x;
-      }
-
-      {
-        int w = threadState->width;
-        int h = threadState->height;
-
-        // static GrGLFramebufferInfo fbInfo = {};
-        // fbInfo.fFBOID = 0u;
-        // fbInfo.fFormat = GL_RGBA8;
-
-        // static GrBackendRenderTarget backendRenderTarget(
-        //     w, h,
-        //     1, 8,
-        //     fbInfo);
-
-        // auto get_proc = [](void *context, const char name[]) -> GrGLFuncPtr
-        // {
-        //   return reinterpret_cast<GrGLFuncPtr>(
-        //       reinterpret_cast<JNIRendererSkia *>(context)->getProcAddress(name));
-        // };
-        // auto get_string =
-        //     reinterpret_cast<PFNGLGETSTRINGPROC>(getProcAddress("glGetString"));
-        // auto c_version = reinterpret_cast<const char *>(get_string(GL_VERSION));
-        // std::string version(c_version);
-        // auto interface = version.find("OpenGL ES") == std::string::npos
-        //                      ? GrGLMakeAssembledGLInterface(this, get_proc)
-        //                      : GrGLMakeAssembledGLESInterface(this, get_proc);
-        // if (!interface)
-        // {
-        //   LOGE("Failed to find the interface version!?");
-        // }
-        // auto gpuContext = GrDirectContext::MakeGL(interface);
-        // if (!gpuContext)
-        // {
-        //   LOGE("Failed to make context!?");
-        // }
-
-        // static SkSurfaceProps surfaceProps(0, kUnknown_SkPixelGeometry);
-
-        // TODO: this *really* needs to be cached. But when I try, it fails.
-        // sk_sp<SkSurface> gpuSurface = SkSurface::MakeFromBackendRenderTarget(
-        //     threadState->getGrContext().get(),
-        //     backendRenderTarget,
-        //     kBottomLeft_GrSurfaceOrigin,
-        //     kN32_SkColorType,
-        //     SkColorSpace::MakeSRGB(),
-        //     &surfaceProps,
-        //     nullptr,
-        //     nullptr);
-
-        auto gpuSurface = threadState->getSkSurface();
-        // auto gpuSurface = threadState->createSkSurface();
-
-        if (!gpuSurface)
+        if (mInstance)
         {
-          LOGE("Failed to get GPU Surface?!");
+          mInstance->advance(elapsed);
+          mInstance->apply(mArtboard);
         }
-        else
-        {
-          SkCanvas *gpuCanvas = gpuSurface->getCanvas();
-          // gpuCanvas->save();
-          // const SkScalar scale = 256.0f;
-          // const SkScalar R = 0.45f * scale;
-          // const SkScalar TAU = 6.2831853f;
-          // SkPath path;
-          // for (int i = 0; i < 5; ++i)
-          // {
-          //   SkScalar theta = 2 * i * TAU / 5;
-          //   if (i == 0)
-          //   {
-          //     path.moveTo(R * cos(theta), R * sin(theta));
-          //   }
-          //   else
-          //   {
-          //     path.lineTo(R * cos(theta), R * sin(theta));
-          //   }
-          // }
-          // path.close();
-          // static float elapsed = 0.0;
-          // elapsed -= deltaSeconds;
-          // float zeroToOne = abs(sin(elapsed));
-          // path.transform(SkMatrix::RotateDeg(zeroToOne * 360.0f));
-          // path.transform(SkMatrix::Translate(w * zeroToOne, h * zeroToOne));
-          // SkPaint p;
-          // p.setAntiAlias(true);
-          // gpuCanvas->clear(SK_ColorWHITE);
-          // gpuCanvas->translate(0.5f * scale, 0.5f * scale);
-          // gpuCanvas->drawPath(path, p);
-          // gpuCanvas->flush();
-          // gpuCanvas->restore();
-          
-          float elapsed = -1.0f * deltaSeconds;
-          gpuCanvas->drawColor(SK_ColorTRANSPARENT, SkBlendMode::kClear);
-          if (mArtboard != nullptr)
-          {
-            if (mInstance)
-            {
-              mInstance->advance(elapsed);
-              mInstance->apply(mArtboard);
-            }
-            mArtboard->advance(elapsed);
-            mSkRenderer->save();
-            mSkRenderer->align(
-                rive::Fit::contain,
-                rive::Alignment::center,
-                rive::AABB(0, 0, w, h),
-                mArtboard->bounds());
-            mArtboard->draw(mSkRenderer);
-            mSkRenderer->restore();
-          }
-
-          threadState->getGrContext()->flush();
-        }
+        mArtboard->advance(elapsed);
+        mSkRenderer->save();
+        mSkRenderer->align(
+            rive::Fit::contain,
+            rive::Alignment::center,
+            rive::AABB(0, 0, w, h),
+            mArtboard->bounds());
+        mArtboard->draw(mSkRenderer);
+        mSkRenderer->restore();
       }
+
+      threadState->getGrContext()->flush();
       // const float aspectRatio = static_cast<float>(threadState->width) / threadState->height;
 
-      // glClearColor(0.7f, 0.5f, 0.3f, 1.0f);
-      // glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
       if (mSwappyEnabled)
       {
         SwappyGL_swap(threadState->display, threadState->surface);
@@ -438,34 +299,6 @@ namespace rive_android
       // If we're still started, request another frame
       requestDraw();
     }
-
-#define CASE_STR(value) \
-  case value:           \
-    return #value;
-    const char *eglGetErrorString(EGLint error)
-    {
-      switch (error)
-      {
-        CASE_STR(EGL_SUCCESS)
-        CASE_STR(EGL_NOT_INITIALIZED)
-        CASE_STR(EGL_BAD_ACCESS)
-        CASE_STR(EGL_BAD_ALLOC)
-        CASE_STR(EGL_BAD_ATTRIBUTE)
-        CASE_STR(EGL_BAD_CONTEXT)
-        CASE_STR(EGL_BAD_CONFIG)
-        CASE_STR(EGL_BAD_CURRENT_SURFACE)
-        CASE_STR(EGL_BAD_DISPLAY)
-        CASE_STR(EGL_BAD_SURFACE)
-        CASE_STR(EGL_BAD_MATCH)
-        CASE_STR(EGL_BAD_PARAMETER)
-        CASE_STR(EGL_BAD_NATIVE_PIXMAP)
-        CASE_STR(EGL_BAD_NATIVE_WINDOW)
-        CASE_STR(EGL_CONTEXT_LOST)
-      default:
-        return "Unknown";
-      }
-    }
-#undef CASE_STR
 
     int width() const
     {
