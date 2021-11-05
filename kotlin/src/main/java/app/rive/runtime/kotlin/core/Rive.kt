216a2eb67ece2f10d76bfedf14c85ff77956b7f4
Umberto Sonnino <umberto@rive.app>
2021-11-05 11:33:15 +0000

    Add a few more checks for loading the library locally

--- kotlin/src/main/java/app/rive/runtime/kotlin/core/Rive.kt
+++ kotlin/src/main/java/app/rive/runtime/kotlin/core/Rive.kt
@@ -1,12 +1,13 @@
 package app.rive.runtime.kotlin.core
 
 import android.annotation.SuppressLint
-import android.annotation.TargetApi
 import android.content.Context
 import android.os.Build
 import android.util.Log
-import java.io.*
 import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
 import java.util.zip.ZipFile
 
 object Rive {
@@ -18,7 +19,44 @@ object Rive {
         requiredBoundsPointer: Long
     )
 
-    private const val JNIRiveBirdge = "jnirivebridge"
+    private const val JNIRiveBridge = "jnirivebridge"
+    private const val JNIRiveBridgeSO = "lib${JNIRiveBridge}.so"
+
+    private var libLoaded = false
+    private var apkFile: ZipFile? = null
+
+    private fun getApkFile(context: Context): ZipFile {
+        if (apkFile != null) {
+            return apkFile!!
+        }
+
+        val apkLocation = context.applicationInfo.sourceDir
+        val file = File(apkLocation)
+        if (!file.exists()) {
+            Log.e("Rive", "No apk file for this Activity?!")
+            throw IOException("Missing APK file in this context $apkLocation")
+        }
+
+        // Return and assign.
+        return ZipFile(apkLocation).also { apkFile = it }
+    }
+
+
+    private val abiCount: Int
+        get() = when {
+            // Do we need this? We're already restricting to be SDK >= 21
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+            -> Build.SUPPORTED_ABIS.size
+            else -> 1
+        }
+
+    private fun getAbiName(index: Int): String {
+        // Do we need this? We're already restricting to be SDK >= 21
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Build.SUPPORTED_ABIS[index]
+        } else Build.CPU_ABI
+    }
+
 
     /**
      * Initialises Rive.
@@ -29,7 +67,7 @@ object Rive {
      */
     fun init(context: Context) {
         try {
-            System.loadLibrary(JNIRiveBirdge)
+            System.loadLibrary(JNIRiveBridge)
         } catch (err: UnsatisfiedLinkError) {
             extractLib(context)
         }
@@ -37,38 +75,128 @@ object Rive {
     }
 
     @SuppressLint("UnsafeDynamicallyLoadedCode")
-    private fun extractFile(inputStream: InputStream, destPath: String) {
-        val outStream = BufferedOutputStream(FileOutputStream(destPath))
+    private fun extractLib(context: Context) {
+        var i = 0
+        val maxTries = abiCount
+        while (!libLoaded && i < maxTries) {
+            val destDir = File(context.filesDir, "lib")
+            destDir.mkdirs()
+
+            val destLocalFile = File(destDir, "lib${JNIRiveBridge}loc.so")
+            if (destLocalFile.exists()) {
+                try {
+                    System.load(destLocalFile.absolutePath)
+                    libLoaded = true
+                    return
+                } catch (_: Error) {
+                }
+                destLocalFile.delete()
+            }
+
+            try {
+                destDir.listFiles()?.forEach { it.delete() }
+            } catch (_: Exception) {
+            }
+
+            val abiName = getAbiName(i)
+            val abiFolder = getFolderFrom(abiName)
+            try {
+                if (loadFromZip(context, destLocalFile, abiFolder)) {
+                    if (libLoaded) return
+                }
+            } catch (_: Exception) {
+            }
+
+            i++
+        }
+
+        // Don't give up.
+        try {
+            System.loadLibrary(JNIRiveBridge)
+            libLoaded = true
+        } catch (e: Error) {
+            e.printStackTrace()
+        }
+
+        if (!libLoaded) {
+            Log.e("Rive", "Failed to initialize JNI Rive Bridge")
+        }
+    }
+
+    private fun getFolderFrom(abi: String): String {
+        var folder: String
+        folder = when {
+            abi.equals("x86_64", true) -> {
+                "x86_64"
+            }
+            abi.equals("arm64-v8a", true) -> {
+                "arm64-v8a"
+            }
+            abi.equals("armeabi-v7a", true) -> {
+                "armeabi-v7a"
+            }
+            abi.equals("armeabi", true) -> {
+                "armeabi"
+            }
+            abi.equals("x86", true) -> {
+                "x86"
+            }
+            abi.equals("mips", true) -> {
+                "mips"
+            }
+            else -> {
+                "armeabi"
+            }
+        }
+
+        val sysArch = System.getProperty("os.arch")
+        if (sysArch != null) if (sysArch.contains("686") || sysArch.contains("x86")) {
+            folder = "x86"
+        }
+        return folder
+    }
+
+
+    @Throws(IOException::class)
+    @SuppressLint("UnsafeDynamicallyLoadedCode")
+    private fun loadFromZip(context: Context, destLocalFile: File, abiFolder: String): Boolean {
+        val apkFile = getApkFile(context)
+        val libEntryLocation = "lib/$abiFolder/$JNIRiveBridgeSO"
+        val libEntry = apkFile.getEntry(libEntryLocation)
+        if (libEntry == null) {
+            apkFile.close()
+            throw IOException("No $libEntryLocation in apkFile")
+        }
+        val stream = apkFile.getInputStream(libEntry)
+
+        writeLibStream(stream, destLocalFile)
+        return try {
+            System.load(destLocalFile.absolutePath)
+            libLoaded = true
+            true
+        } catch (_: Error) {
+            false
+        } finally {
+            // Clean up resources before returning.
+            stream.close()
+            apkFile.close()
+        }
+    }
+
+    @SuppressLint("SetWorldReadable")
+    private fun writeLibStream(inputStream: InputStream, destLocalFile: File) {
+        val outputStream = FileOutputStream(destLocalFile)
         val bytesArray = ByteArray(4096)
         var read: Int
         while (inputStream.read(bytesArray).also { read = it } != -1) {
-            outStream.write(bytesArray, 0, read)
+            Thread.yield()
+            outputStream.write(bytesArray, 0, read)
         }
-        outStream.close()
-        System.load(destPath)
+        outputStream.close()
+        destLocalFile.setReadable(true, false)
+        destLocalFile.setExecutable(true, false)
+        destLocalFile.setWritable(true)
     }
-
-    private fun extractLib(context: Context) {
-        val apkLocation = context.applicationInfo.sourceDir
-        val apkFile = File(apkLocation)
-        if (!apkFile.exists()) {
-            Log.e("Rive", "No apk file for this activity?!")
-            throw IOException("Couldn't find the apk file?!")
-        }
-        val apkZip = ZipFile(apkLocation)
-        apkZip.use { archive ->
-            archive.entries().asSequence().first { entry ->
-                entry.name.contains(JNIRiveBirdge)
-            }.let { lib ->
-                val libStream = archive.getInputStream(lib)
-                val dataDir = context.applicationInfo.dataDir
-                val libName = lib.name
-                val filename = libName.substring(libName.lastIndexOf('/') + 1)
-                extractFile(libStream, "$dataDir/$filename")
-            }
-        }
-    }
-
 
     fun calculateRequiredBounds(
         fit: Fit,
@@ -86,5 +214,4 @@ object Rive {
         )
         return requiredBounds
     }
-
 }
