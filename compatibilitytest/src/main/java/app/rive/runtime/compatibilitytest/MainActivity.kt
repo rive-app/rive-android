515e7de2d134fe9822bb5770ae2fa53aa0b6cdf0
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-11-24 12:33:39 +0000

    adding compat test app

---
+++ compatibilitytest/src/main/java/app/rive/runtime/compatibilitytest/MainActivity.kt
@@ -0,0 +1,14 @@
+package app.rive.runtime.compatibilitytest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.rive.runtime.kotlin.core.Rive
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Rive.init(applicationContext)
+        setContentView(R.layout.activity_main)
+
+    }
+}
