c42c958138ac623e48355febe58f1fefcef4ea63
Maxwell Talbot <talbot.maxwell@gmail.com>
2021-11-26 14:43:44 +0000

    bit of housekeeping, moving the fps counter to metrics in anticipation

--- app/src/main/java/app/rive/runtime/example/MetricsActivity.kt
+++ app/src/main/java/app/rive/runtime/example/MetricsActivity.kt
@@ -11,18 +11,32 @@ class MetricsActivity : AppCompatActivity() {
     private val containerView by lazy(LazyThreadSafetyMode.NONE) {
         findViewById<LinearLayout>(R.id.container)
     }
+//
+//    private fun addSwappyView() {
+//        findViewById(R.id.swappy_view)
+//            ?: RiveAnimationView(this, null).also {
+//                val density = resources.displayMetrics.density
+//                it.layoutParams = LinearLayout.LayoutParams(
+//                    LinearLayout.LayoutParams.MATCH_PARENT,
+//                    (366 * density).toInt(),
+//                )
+//                containerView.addView(it, 0)
+//
+//            }
+//    }
 
-    private fun addSwappyView() {
-        findViewById(R.id.swappy_view)
-            ?: RiveAnimationView(this, null).also {
-                val density = resources.displayMetrics.density
-                it.layoutParams = LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    (366 * density).toInt(),
-                )
-                containerView.addView(it, 0)
-            }
-    }
+//    private fun updateFps() {
+//
+//        val fpsView = findViewById<TextView>(R.id.fps)
+//        val fps = nGetAverageFps(riveRenderer.address)
+//        fpsView?.text =
+//            java.lang.String.format(
+//                Locale.US,
+//                "Frame rate: %.1f Hz (%.2f ms)",
+//                fps,
+//                1e3f / fps
+//            )
+//    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +45,6 @@ class MetricsActivity : AppCompatActivity() {
         Rive.init(this)
         setContentView(R.layout.activity_metrics)
         // Make sure this is initialized.
-        addSwappyView()
+//        addSwappyView()
     }
 }
