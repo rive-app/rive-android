23ac282200e3ea0ab3a1a3904f10ae1e5f82e024
Umberto Sonnino <umberto@rive.app>
2022-03-17 10:28:56 +0000

    Add new activity with mesh example
    Refactor button listener generation to be a bit more compact

---
+++ app/src/main/java/app/rive/runtime/example/MeshesActivity.kt
@@ -0,0 +1,11 @@
+package app.rive.runtime.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MeshesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meshes)
+    }
+}
