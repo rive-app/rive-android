e73d5bf6280ab140dce46c4e571661ed7b326614
Maxwell Talbot <talbot.maxwell@gmail.com>
2022-06-13 15:26:00 +0100

    add jpeg support and rev cpp for memory fixes

--- cpp/make_skia_android.sh
+++ cpp/make_skia_android.sh
@@ -60,7 +60,6 @@ bin/gn gen out/"${CONFIG}"/"${ARCH}" --args=" \
     skia_use_libpng_decode=true \
     skia_use_libpng_encode=true \
 
-
     skia_use_angle=false \
     skia_use_dng_sdk=false \
     
@@ -70,8 +69,9 @@ bin/gn gen out/"${CONFIG}"/"${ARCH}" --args=" \
     skia_use_icu=false \
     skia_use_libheif=false \
     skia_use_system_libpng=false \
+    skia_use_system_libjpeg_turbo=false \
     skia_use_libjpeg_turbo_encode=false \
-    skia_use_libjpeg_turbo_decode=false \
+    skia_use_libjpeg_turbo_decode=true \
     skia_use_libwebp_encode=false \
     skia_use_libwebp_decode=false \
     skia_use_lua=false \
