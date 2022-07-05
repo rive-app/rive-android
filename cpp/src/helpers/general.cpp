da7d308f880224e2e6567003be9796773ed916c6
mikerreed <hello@rive.app>
2022-07-05 15:49:26 +0000

    Use BitmapFactory for platform decoding
    Extends SkiaFactory to call into Java to use Android's BitmapFactory to try to decode an image. This is called by SkiaFactory's base implementation when Skia itself fails to decode an image.
    
    Android has a NDK api for image-decoding, and it looks like something we could use. It should be more efficient than this code, since it (1) avoids JNI up and down calls, and avoids copies of the input and output data. However, it is only available since SDK 30, and currently we support devices older than that.
    
    Diffs=
    06f0b2c28 Use BitmapFactory for runtime decoding

--- cpp/src/helpers/general.cpp
+++ cpp/src/helpers/general.cpp
@@ -1,8 +1,8 @@
 #include "jni_refs.hpp"
 #include "helpers/general.hpp"
-#include "skia_factory.hpp"
 #include "rive/file.hpp"
 #include "rive/layout.hpp"
+#include "skia_factory.hpp"
 
 #ifdef DEBUG
 #include <errno.h>
@@ -11,7 +11,96 @@
 #include <EGL/egl.h>
 #endif
 
-static rive::SkiaFactory gFactory;
+class AndroidSkiaFactory : public rive::SkiaFactory {
+public:
+    std::vector<uint8_t> platformDecode(rive::Span<const uint8_t> span,
+                                        rive::SkiaFactory::ImageInfo* info) override {
+	    auto env = rive_android::getJNIEnv();
+        std::vector<uint8_t> pixels;
+
+        jclass cls = env->FindClass("app/rive/runtime/kotlin/core/Decoder");
+        if (!cls) {
+            LOGE("can't find class 'app/rive/runtime/kotlin/core/Decoder'");
+            return pixels;
+        }
+
+        jmethodID method = env->GetStaticMethodID(cls, "decodeToPixels", "([B)[I");
+        if (!method) {
+            LOGE("can't find static method decodeToPixels");
+            return pixels;
+        }
+
+        jbyteArray encoded = env->NewByteArray(span.size());
+        if (!encoded) {
+            LOGE("failed to allcoate NewByteArray");
+            return pixels;
+        }
+
+        env->SetByteArrayRegion(encoded, 0, span.size(), (jbyte*)span.data());
+        auto jpixels = (jintArray)env->CallStaticObjectMethod(cls, method, encoded);
+        env->DeleteLocalRef(encoded);  // no longer need encoded
+
+        // At ths point, we have the decode results. Now we just need to convert
+        // it into the form we need (ImageInfo + premul pixels)
+
+        size_t arrayCount = env->GetArrayLength(jpixels);
+        if (arrayCount < 2) {
+            LOGE("bad array length (unexpected)");
+            return pixels;
+        }
+
+        int* rawPixels = env->GetIntArrayElements(jpixels, nullptr);
+        const uint32_t width = rawPixels[0];
+        const uint32_t height = rawPixels[1];
+        const size_t pixelCount = (size_t)width * height;
+        if (pixelCount == 0) {
+            LOGE("don't support empty images (zero dimension)");
+            return pixels;
+        }
+        if (2 + pixelCount < arrayCount) {
+            LOGE("not enough elements in pixel array");
+            return pixels;
+        }
+
+        auto div255 = [](unsigned value) {
+            return (value + 128) * 257 >> 16;
+        };
+
+        pixels.resize(pixelCount * 4);
+        uint8_t* bytes = pixels.data();
+        bool isOpaque = true;
+        for (size_t i = 0; i < pixelCount; ++i) {
+            uint32_t p = rawPixels[2 + i];
+            unsigned a = (p >> 24) & 0xFF;
+            unsigned r = (p >> 16) & 0xFF;
+            unsigned g = (p >>  8) & 0xFF;
+            unsigned b = (p >>  0) & 0xFF;
+            // convert to premul as needed
+            if (a != 255) {
+                r = div255(r * a);
+                g = div255(g * a);
+                b = div255(b * a);
+                isOpaque = false;
+            }
+            bytes[0] = r;
+            bytes[1] = g;
+            bytes[2] = b;
+            bytes[3] = a;
+            bytes += 4;
+        }
+        env->ReleaseIntArrayElements(jpixels, rawPixels, 0);
+
+        info->rowBytes = width * 4; // we're snug
+        info->width = width;
+        info->height = height;
+        info->colorType = ColorType::rgba;
+        info->alphaType = isOpaque ? AlphaType::opaque
+                                   : AlphaType::premul;
+        return pixels;
+    }
+};
+
+static AndroidSkiaFactory gFactory;
 
 // luigi: murdered this due to our single renderer model right now...all canvas
 // rendering won't work in this branch lets make sure we stich our rive android
@@ -160,8 +249,7 @@ namespace rive_android
 		return alignment;
 	}
 
-	long import(uint8_t* bytes, jint length)
-	{
+	long import(uint8_t* bytes, jint length) {
 		rive::ImportResult result;
 		auto file = rive::File::import(rive::Span<const uint8_t>(bytes, length),
 		                               &gFactory,
---
+++ kotlin/src/main/java/app/rive/runtime/kotlin/core/Decoder.java
@@ -0,0 +1,33 @@
+package app.rive.runtime.kotlin.core;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class Decoder {
+    // output array caries width,height in the first two slots:
+    //  output[0] is width
+    //  output[1] is height
+    //  output[2...] are the pixel values
+    //
+    static int[] decodeToPixels(byte[] encoded) {
+        try {
+            BitmapFactory.Options opts = new BitmapFactory.Options();
+            opts.inMutable = true;   // since we want to read its pixels
+            opts.inScaled = false;   // we handle scaling at draw-time
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encoded, 0, encoded.length, opts);
+
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            int offset = 2; // [0] is width, [1] is height
+            int[] pixels = new int[offset + width * height];
+            bitmap.getPixels(pixels, offset, width,0, 0, width, height);
+
+            pixels[0] = width;
+            pixels[1] = height;
+            return pixels;  // bgra unpremul with stride == width
+        } catch (Exception e) {
+            // fall out
+        }
+        return new int[0]; // failed to decode
+    }
+}
